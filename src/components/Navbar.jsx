0771ccb2b9626fcf195e41625991e9af45849c30
Kevin <trupiakevin@gmail.com>
2026-01-28 16:46:34 +0100

    fix : components nella cartella pages feat: creazione cpmponents navbar e costruzione della navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/about">Chi siamo</Link> |{" "}
+            <Link to="/products">Prodotti</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
