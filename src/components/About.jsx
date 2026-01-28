02f4bc17a2c51fe14a67e4b70cff43c5c717a57f
Kevin <trupiakevin@gmail.com>
2026-01-28 16:00:58 +0100

    feat : installazzione dei package , creazione cartella components e creazione dei file components

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>Chi siamo</h1>
+            <p>Siamo un mini e-commerce creato con React</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Benvenuto nel nostro Store</h1>
+            <p>Qui trovi i migliori prodotti online</p>
+        </div>
+    );
+}
+
+export default Home;
