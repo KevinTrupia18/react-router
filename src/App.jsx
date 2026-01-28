d235b5aa645068c652efba85c0a2ac045c6e5cb2
Kevin <trupiakevin@gmail.com>
2026-01-28 17:59:47 +0100

    feat : task completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
 
-    </>
-  )
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
